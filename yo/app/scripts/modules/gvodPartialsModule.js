ac25fddb68cd4da50aafa15ddb0b179ccad13a38
jsvhqr <jsvhqr@yahoo.com>
2016-04-25 14:00:51 +0200

    changes

--- yo/app/scripts/modules/gvodPartialsModule.js
+++ yo/app/scripts/modules/gvodPartialsModule.js
@@ -4,77 +4,91 @@
  * and open the template in the editor.
  */
 
-angular.module('partialsApplication',[]);
+angular.module('partialsApplication', []);
 
-angular.module('partialsApplication').controller('Downloader', ['BackendService',function(BackendService){
-        
+angular.module('partialsApplication').controller('Downloader', ['BackendService', function (BackendService) {
+
         var self = this;
         self.filename;
         self.identifier;
         self.result;
 
-        
-        
-        self.download = function(){
-            var JSONObj = { "name ":self.filename, "identifier":self.identifier };
-            BackendService.download(JSONObj).then(function(result){
+
+
+        self.download = function () {
+            var JSONObj = {"name ": self.filename, "identifier": self.identifier};
+            BackendService.download(JSONObj).then(function (result) {
                 self.result = result;
             });
-            
+
         };
-        
-}]);
-angular.module('partialsApplication').controller('Uploader', ['BackendService',function(BackendService){
-        
+
+    }]);
+angular.module('partialsApplication').controller('Uploader', ['BackendService', function (BackendService) {
+
         var self = this;
         self.filename;
         self.identifier;
         self.result;
 
-        
-        
-        self.upload = function(){
-            var JSONObj = { "name ":self.filename, "identifier":self.identifier };
-            BackendService.upload(JSONObj).then(function(result){
+
+
+        self.upload = function () {
+            var JSONObj = {"name ": self.filename, "identifier": self.identifier};
+            BackendService.upload(JSONObj).then(function (result) {
                 self.result = result;
             });
-            
+
         };
-        
-}]);
-angular.module('partialsApplication').controller('Stoper', ['BackendService',function(BackendService){
-        
+
+    }]);
+angular.module('partialsApplication').controller('Stoper', ['BackendService', function (BackendService) {
+
         var self = this;
         self.filename;
         self.identifier;
         self.result;
 
-        
-        
-        self.stop = function(){
-            var JSONObj = { "name ":self.filename, "identifier":self.identifier };
-            BackendService.stop(JSONObj).then(function(result){
+
+
+        self.stop = function () {
+            var JSONObj = {"name ": self.filename, "identifier": self.identifier};
+            BackendService.stop(JSONObj).then(function (result) {
                 self.result = result;
             });
-            
+
         };
-        
-}]);
-angular.module('partialsApplication').factory('BackendService',['$http',function($http){
+
+    }]);
+angular.module('partialsApplication').factory('BackendService', ['$http', function ($http) {
         var service = {
-            
             download: function (json) {
-                  return $http('http://localhost:18180/torrent/download',json);
-                },
+                return $http(
+                        {
+                            method: 'PUT',
+                            url: 'http://localhost:18180/torrent/download',
+                            data: json
+                        });
+            },
             upload: function (json) {
-                  return $http.put('http://localhost:18180/torrent/upload',json);
-                },
+                return $http(
+                        {
+                            method: 'PUT',
+                            url: 'http://localhost:18180/torrent/upload',
+                            data: json
+                        });
+            },
             stop: function (json) {
-                  return $http.put('http://localhost:18180/torrent/stop',json);
-                }    
-            
+                return $http(
+                        {
+                            method: 'PUT',
+                            url: 'http://localhost:18180/torrent/stop',
+                            data: json
+                        });
+            }
+
         };
-        
+
         return service;
-}]);
+    }]);
 

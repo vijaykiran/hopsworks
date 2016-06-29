f7cdad10d226878e805eb1c9ea4b0dfc5cd1c5fc
jsvhqr <jsvhqr@yahoo.com>
2016-06-29 11:51:24 +0200

    updates

---
+++ src/main/java/se/kth/hopsworks/gvod/gvodController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.hopsworks.gvod;
+
+/**
+ *
+ * @author jsvhqr
+ */
+public class gvodController {
+    
+    
+    
+    public void uploadToGvod(String ip, int port, String path, String filename, String user, String torrentId){
+        
+        
+        
+    }
+    
+    
+}

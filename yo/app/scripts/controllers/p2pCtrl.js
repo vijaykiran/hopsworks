12ab0f844c6498c94ab003c101145c7fe502946e
jsvhqr <jsvhqr@yahoo.com>
2016-08-28 15:20:14 +0200

    updates

--- yo/app/scripts/controllers/p2pCtrl.js
+++ yo/app/scripts/controllers/p2pCtrl.js
@@ -1,44 +1,46 @@
-angular.module('hopsWorksApp').controller('P2PCtrl', ['GVoDService','$routeParams','$scope','$interval', function (GVoDService, $routeParams, $scope, $interval) {
+angular.module('hopsWorksApp').controller('P2PCtrl', ['GVoDService', '$routeParams', '$scope', '$interval', function (GVoDService, $routeParams, $scope, $interval) {
         var self = this;
-      
+
         self.projectId = $routeParams.projectID;
-        
+
         self.contents;
-        
-        self.remove = function(torrentId){
+
+        self.remove = function (torrentId) {
             var json = {};
             json.torrentId = torrentId;
-            GVoDService.remove(json).then(function(success){
-               getContents(); 
+            GVoDService.remove(json).then(function (success) {
+                getContents();
             });
         };
-        
-        var getContents = function(){
-          
+
+        var getContents = function () {
+
             var json = {};
             json.projectId = self.projectId;
-            
-            GVoDService.getContents(json).then(function(success){
+
+            GVoDService.getContents(json).then(function (success) {
                 self.contents = success.data.contents;
-                for(var i = 0; i<self.contents.length;i++){
-                    var json = {};
-                    json.torrentId = self.contents[i].torrentId;
-                    GVoDService.getExtendedDetails(json).then(function(success){
-                        self.contents[i].progress = success.data.percentageCompleted;
+                var length = self.contents.length;
+                for (var j = 0; j < length; j++) {
+                    self.contents[j]['max'] = 100;
+                    var ejson = {};
+                    ejson.torrentId = self.contents[j].torrentId.val;
+                    GVoDService.getExtendedDetails(ejson).then(function (success) {
+                        self.contents[j]['dynamic'] = success.percentageCompleted * 100;
                     });
                 }
             });
-            
+
         };
-        
+
         getContents();
-        
-        
+
+
         var contentsInterval = $interval(function () {
-                     getContents();
-                }, 1000);
-                
+            getContents();
+        }, 5000);
+
         $scope.$on("$destroy", function () {
-                    $interval.cancel(contentsInterval);
-                });        
+            $interval.cancel(contentsInterval);
+        });
     }]);
